d462897c0e3afe89a0813d00eaa96979b360ceee
Matheus Fidelis <msfidelis01@gmail.com>
2018-06-02 22:37:01 -0300

    remove aws_key

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-
 provider "aws" {
-    region = "${var.aws_region}"
-}
-
-resource "aws_key_pair" "vpc_terraform_key" {
-  key_name = "vpc-terraform-key"
-  public_key = "${file("${var.aws_key_path}")}"
+  region = "${var.aws_region}"
 }
--- variables.tf
+++ variables.tf
@@ -2,30 +2,12 @@
 ###### QUICK EDIT'S HERE  ######
 ################################
 
-# Customize your ECS Cluster Options
+###### CLUSTER OPTIONS  ######
+
+# Customize the Cluster Name
 variable "cluster_name" {
   description = "ECS Cluster Name"
   default     = "web-app"
-}
-
-variable "container_name" {
-  description = "Container app name"
-  default     = "micro-api"
-}
-
-variable "desired_tasks" {
-  description = "Number of containers desired to run app task"
-  default     = 3
-}
-
-variable "desired_task_cpu" {
-  description = "Desired CPU to run your tasks"
-  default     = "256"
-}
-
-variable "desired_task_memory" {
-  description = "Desired memory to run your tasks"
-  default     = "512"
 }
 
 # Customize your ECR Registry Name
@@ -33,6 +15,32 @@ variable "app_repository_name" {
   description = "ECR Repository Name"
   default     = "web-app"
 }
+
+###### APPLICATION OPTIONS  ######
+variable "container_name" {
+  description = "Container app name"
+  default     = "micro-api"
+}
+
+# Number of containers
+variable "desired_tasks" {
+  description = "Number of containers desired to run app task"
+  default     = 2
+}
+
+# Desired CPU
+variable "desired_task_cpu" {
+  description = "Desired CPU to run your tasks"
+  default     = "256"
+}
+
+# Desired Memory
+variable "desired_task_memory" {
+  description = "Desired memory to run your tasks"
+  default     = "512"
+}
+
+###### GITHUB OPTIONS  ######
 
 # Github Repository Owner
 variable "git_repository_owner" {
@@ -62,12 +70,6 @@ variable "alb_listener_port" {
 variable "alb_target_port" {
   description = "Destination Application Load Balancer Port"
   default     = "80"
-}
-
-# Optional - For Classic ECS Only
-variable "aws_key_path" {
-  description = "Public key path"
-  default     = "/Users/matheus/.ssh/id_rsa.pub"
 }
 
 # Customize your AWS Region
