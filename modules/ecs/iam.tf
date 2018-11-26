e481e9333447e5d9d895215158de23d5c40d51ae
Matheus Fidelis <msfidelis@outlook.com>
2018-11-26 15:57:32 -0200

    roles

--- modules/ecs/iam.tf
+++ modules/ecs/iam.tf
@@ -1,23 +1,23 @@
 # Cluster Execution Role
 resource "aws_iam_role" "ecs_execution_role" {
-  name               = "ecs_task_execution_role"
+  name               = "${var.cluster_name}-ecs_task_role"
   assume_role_policy = "${file("${path.module}/policies/ecs-task-execution-role.json")}"
 }
 
 # Cluster Execution Policy
 resource "aws_iam_role_policy" "ecs_execution_role_policy" {
-  name   = "ecs_execution_role_policy"
+  name   = "${var.cluster_name}_role_policy"
   policy = "${file("${path.module}/policies/ecs-execution-role-policy.json")}"
   role   = "${aws_iam_role.ecs_execution_role.id}"
 }
 
 resource "aws_iam_role" "ecs_role" {
-  name               = "ecs_role"
+  name               = "${var.cluster_name}-ecs_role"
   assume_role_policy = "${data.aws_iam_policy_document.ecs_service_role.json}"
 }
 
 resource "aws_iam_role_policy" "ecs_service_role_policy" {
-  name   = "ecs_service_role_policy"
+  name   = "${var.cluster_name}_role_policy"
   policy = "${data.aws_iam_policy_document.ecs_service_policy.json}"
   role   = "${aws_iam_role.ecs_role.id}"
 }
